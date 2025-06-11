38535e0937adaf926942ab762adb759abe70437a
123 <123.@qq.com>
2025-06-12 05:43:08 +0800

    苍穹外卖插入员工功能实现

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -73,6 +73,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateUser(BaseContext.getCurrentId());
         employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.insert(employee);
+
     }
 
 }
